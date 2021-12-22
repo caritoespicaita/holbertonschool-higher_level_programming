d4ee1cf3776306b76ce970c8aed313761f393aa1
caritoespicaita <caritoespicaita@hotmail.com>
2021-12-21 19:15:20 -0500

    Update task 0.

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+   rta = "is positive"
+elif number == 0:
+    rta = "is zero"
+else:
+    rta = "is negative"
+print ("{:d}".format(number),rta)
+    
