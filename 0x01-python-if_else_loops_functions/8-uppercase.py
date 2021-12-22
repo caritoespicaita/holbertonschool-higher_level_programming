a4a275d5126df5c80248ebec67818462038f621f
caritoespicaita <caritoespicaita@hotmail.com>
2021-12-22 00:14:15 -0500

    Update task 8.

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for upper in str:
+        if ord(upper) >= 97 and ord(upper) <= 122:
+            letter = ord(upper) - 32
+        else:
+            letter = ord(upper)
+        print("{}".format(chr(letter)), end="")
+    print("")
+   
