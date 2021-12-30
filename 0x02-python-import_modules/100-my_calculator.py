0edd251a7ace710bfb129757c48fe5cf181a18f9
caritoespicaita <caritoespicaita@hotmail.com>
2021-12-30 10:58:44 -0500

    fix code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,17 +6,18 @@ if __name__ == "__main__":
     if len != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif argv[2] == '+':
-        res = add(int(argv[1]), int(argv[3]))
-    elif argv[2] == '-':
-        res = sub(int(argv[1]), int(argv[3]))
-    elif argv[2] == '*':
-        res = mul(int(argv[1]), int(argv[3]))
-    elif argv[2] == '/':
-        res = div(int(argv[1]), int(argv[3]))
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+    if operator == '+':
+        res = add(a, b)
+    elif operator == '-':
+        res = sub(a, b)
+    elif operator == '*':
+        res = mul(a, b)
+    elif operator == '/':
+        res = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    print("{:d} {:} {:d} = {:d}".format(a, argv[2], b, res))
+    print("{:d} {:} {:d} = {:d}".format(a, operator, b, res))
