a1e0821a6ca67f4943c4bf07ff104aba7e5018ce
caritoespicaita <caritoespicaita@hotmail.com>
2022-01-04 18:17:51 -0500

    Update task 6.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        space = ''
+        for k in i:
+            print('{}{:d}'.format(space, k), end='')
+            space = ' '
+        print('')
