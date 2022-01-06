c8daa6267a98e28a8235ed353b7e0383228cddf2
caritoespicaita <caritoespicaita@hotmail.com>
2022-01-05 20:13:43 -0500

    fix style

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for idx in range(len(my_list)):
-        
-
+    if search in my_list:
+        new_list = my_list.copy()
+        j = 0
+        for i in my_list:
+            if i == search:
+                new_list[j] = replace
+            j += 1
+    else:
+        new_list = my_list
+    return new_list
