6e42577a477b7560a01331eb8c75e1df6608c40f
caritoespicaita <caritoespicaita@hotmail.com>
2022-01-31 22:43:22 -0500

    fix pycodestyle

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,8 +5,8 @@ This module contains the function inherits_from
 
 
 def inherits_from(obj, a_class):
-    """  returns True if the object is an instance 
-    of a class that inherited (directly or indirectly) 
+    """  returns True if the object is an instance
+    of a class that inherited (directly or indirectly)
     from the specified class ; otherwise False.
     """
     return type(obj) != a_class and isinstance(obj, a_class)
