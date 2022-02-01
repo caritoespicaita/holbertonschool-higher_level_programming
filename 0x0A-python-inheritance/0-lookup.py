fc9f593f9dd1a253e3f1535c48f921950bbcc22a
caritoespicaita <caritoespicaita@hotmail.com>
2022-01-31 20:13:23 -0500

    fix pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+""" Funcyion return losts """
 
 
 def lookup(obj):
     """returns all objects in an objects dictionary
         -> as a list
-    """ 
+    """
     return dir(obj)
