78763a7513ad7e5fb4a5583afa2c67f1daca83d2
caritoespicaita <caritoespicaita@hotmail.com>
2022-02-01 21:32:07 -0500

    fix  code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """ open file and print """
     with open(filename, mode="r", encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data)
+        print(f.read(), end="")
