b70326f0c79a885e6e50f484b65e3433f99d0abd
caritoespicaita <caritoespicaita@hotmail.com>
2022-02-02 15:42:30 -0500

    Update task 13.

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3#
+""" function that that inserts a line of text to a file """
+
+
+def append_write(filename="", text=""):
+    """ after each line insert a specific sttring """
+    with open(filename, mode="r", encoding="utf-8") as file:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(line_list)
