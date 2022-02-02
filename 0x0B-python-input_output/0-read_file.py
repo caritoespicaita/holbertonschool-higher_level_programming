314a7a7fb6af5444ee5c409ebe48feb57b2f7826
caritoespicaita <caritoespicaita@hotmail.com>
2022-02-01 21:29:22 -0500

    fix pycodestyle and code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3#
 """ Fuction that reads a tex file """
 
+
 def read_file(filename=""):
     """ open file and print """
-    with open (filename) as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
     f.closed
