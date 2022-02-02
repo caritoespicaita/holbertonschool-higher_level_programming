de1d4d57663b3dac15a4725afbe9dd30cf0ca98e
caritoespicaita <caritoespicaita@hotmail.com>
2022-02-01 23:18:18 -0500

    Update task 6.

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def load_from_json_file(filename):
     """ create json to file """
-    with open(filename, mode="r", encoding="utf-8") as json_file:
+    with open(filename, mode="w", encoding="utf-8") as json_file:
         json_string = json.load(my_obj)
         json_file.write(json_string)
