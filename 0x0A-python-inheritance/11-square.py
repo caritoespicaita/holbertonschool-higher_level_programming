a9b8165f49434555f33e5356ae4f9976cdeca47c
caritoespicaita <caritoespicaita@hotmail.com>
2022-02-02 16:28:05 -0500

    upload task 11.

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry
+subclass Rectangle and subclass Square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+Square = __import__('10-square').Square
+
+
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size):
+        """instantiation of the square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """informal string reepresentation of the square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
