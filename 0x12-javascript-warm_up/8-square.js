900120c9d3b9d81e05e2aece4fd62d496a065561
caritoespicaita <caritoespicaita@hotmail.com>
2022-04-04 22:57:26 -0500

    update task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const args = process.argv[2];
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-    for (let j = 0; j < args; j++) {
-      console.log('X'.repeat(args));
-    }
+  for (let j = 0; j < args; j++) {
+    console.log('X'.repeat(args));
   }
+}
