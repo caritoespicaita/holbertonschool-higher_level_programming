9a2ec38937ed54037cb2c1ba8c0ed7472e2c7b5a
caritoespicaita <caritoespicaita@hotmail.com>
2022-04-04 23:28:15 -0500

    upadate tasks

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+const array = process.argv.slice(2);
+
+if (array.length <= 1) {
+  console.log('0');
+} else {
+  array.sort((a, b) => a - b);
+  const len = array.length;
+  console.log(array[len - 2]);
+}
