b8d4ce6e19f7c18279c392811df4c7f93cf3b508
caritoespicaita <caritoespicaita@hotmail.com>
2022-04-07 23:50:51 -0500

    Update task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 
-const map1 = list.map((num) => num * list.indexOf(num));
-
+const map1 = list.map((num, index) => {
+  return num * index;
+});
 console.log(list);
 console.log(map1);
