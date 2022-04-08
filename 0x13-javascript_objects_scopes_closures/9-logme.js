84377e33bcfc7517aaec99f4c63f8dbc4f77b5d9
caritoespicaita <caritoespicaita@hotmail.com>
2022-04-07 23:13:57 -0500

    Update task 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const list = [];
-exports.logMe = function (item){
-let idx = list.indexOf(item);
-list.push(item);
-idx = list.indexOf(item, idx + 1);
-console.log(idx + ":" + list[idx]);
+exports.logMe = function (item) {
+  let idx = list.indexOf(item);
+  list.push(item);
+  idx = list.indexOf(item, idx + 1);
+  console.log(idx + ':' + list[idx]);
 };
