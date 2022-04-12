09cab04a795bfdec88e41c2d21b442f44fb78906
caritoespicaita <caritoespicaita@hotmail.com>
2022-04-12 10:40:42 -0500

    Update task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (error) console.log(error);
-  console.log("code: " + response.statusCode);
+  console.log("status code: " + response.statusCode);
 });
