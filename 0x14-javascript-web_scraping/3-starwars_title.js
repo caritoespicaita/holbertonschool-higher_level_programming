f409380fe65b84efd57aa6c550f41967752113c7
caritoespicaita <caritoespicaita@hotmail.com>
2022-04-12 12:47:30 -0500

    Update task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request(url, function(err, res, body) {
+    let json = JSON.parse(body)
+    console.log(json.results[process.argv[2]-1].title);
+});
