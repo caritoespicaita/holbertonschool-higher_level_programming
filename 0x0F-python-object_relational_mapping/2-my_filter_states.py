9766bc0139642019db738425c915721c7bf4b06c
caritoespicaita <caritoespicaita@hotmail.com>
2022-05-10 23:56:02 -0500

    upload task 2.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" use the module MySQLdb """
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".
+                format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
+    db.close()
